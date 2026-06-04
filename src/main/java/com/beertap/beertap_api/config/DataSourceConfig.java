80f3ec828e3e55fd3159c75129c1a55c55df4f09
Rafiki-DEV-362 <arnaurafikidev@gmail.com>
2026-06-04 11:22:17 +0200

    Deploy

--- src/main/java/com/beertap/beertap_api/config/DataSourceConfig.java
+++ src/main/java/com/beertap/beertap_api/config/DataSourceConfig.java
@@ -1,5 +1,8 @@
 package com.beertap.beertap_api.config;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
 import com.zaxxer.hikari.HikariDataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,22 +19,27 @@ public class DataSourceConfig {
 
     @Bean
     @ConditionalOnExpression("'${DATABASE_URL:}' != ''")
-    public DataSource postgresDataSource() {
+    public DataSource postgresDataSource() throws URISyntaxException {
         String databaseUrl = System.getenv("DATABASE_URL");
-        String pgUser = System.getenv("PGUSER");
-        String pgPassword = System.getenv("PGPASSWORD");
 
         log.info("Configuring PostgreSQL DataSource from DATABASE_URL");
 
-        if (!databaseUrl.startsWith("jdbc:")) {
-            databaseUrl = "jdbc:" + databaseUrl;
-        }
+        URI uri = new URI(databaseUrl);
+        String host = uri.getHost();
+        int port = uri.getPort();
+        String dbname = uri.getPath().replaceFirst("^/", "");
+        String user = uri.getUserInfo() != null ? uri.getUserInfo().split(":")[0] : null;
+        String password = uri.getUserInfo() != null && uri.getUserInfo().contains(":")
+                ? uri.getUserInfo().split(":")[1] : null;
+
+        String pgUser = System.getenv("PGUSER");
+        String pgPassword = System.getenv("PGPASSWORD");
 
         HikariDataSource ds = new HikariDataSource();
-        ds.setJdbcUrl(databaseUrl);
+        ds.setJdbcUrl("jdbc:postgresql://" + host + ":" + port + "/" + dbname);
         ds.setDriverClassName("org.postgresql.Driver");
-        if (pgUser != null) ds.setUsername(pgUser);
-        if (pgPassword != null) ds.setPassword(pgPassword);
+        ds.setUsername(pgUser != null ? pgUser : user);
+        ds.setPassword(pgPassword != null ? pgPassword : password);
         ds.setMaximumPoolSize(10);
         return ds;
     }
